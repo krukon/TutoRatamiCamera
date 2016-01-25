fb0f2de3a89c6e6395eada11792f73c30da0d719
Wojtek Kruk <krukon@gmail.com>
2016-01-25 22:52:54 +0100

    Corrected and optimized edge filter

--- app/src/main/java/com/github/krukon/tutoratamicamera/effects/EdgeFilter.java
+++ app/src/main/java/com/github/krukon/tutoratamicamera/effects/EdgeFilter.java
@@ -3,7 +3,6 @@ package com.github.krukon.tutoratamicamera.effects;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.renderscript.Allocation;
-import android.renderscript.Element;
 
 import com.github.krukon.tutoratamicamera.ScriptC_edge;
 
@@ -18,24 +17,10 @@ public class EdgeFilter extends AbstractFilter {
         super(imageWidth, imageHeight, context);
 
         script = new ScriptC_edge(rs);
-
-        int[] row_indices = new int[imageHeight];
-        for (int i = 0; i < imageHeight; i++) {
-            row_indices[i] = i * imageWidth;
-        }
-        Allocation row_indices_alloc = Allocation.createSized(rs, Element.I32(rs), imageHeight, Allocation.USAGE_SCRIPT);
-        row_indices_alloc.copyFrom(row_indices);
-
-        //TODO - why cant I use imageWidth, imageHeight???
-        script.set_imageWidth(1100);
-        script.set_imageHeight(1080);
-
-        script.set_in(row_indices_alloc);
-        script.set_out(row_indices_alloc);
-
-        script.bind_inPixels(allocationIn);
-        script.bind_outPixels(allocationOut);
-
+        script.set_imageWidth(imageWidth);
+        script.set_imageHeight(imageHeight);
+        script.set_in(allocationIn);
+        script.set_out(allocationOut);
         script.set_script(script);
     }
 
--- app/src/main/rs/edge.rs
+++ app/src/main/rs/edge.rs
@@ -9,37 +9,35 @@ rs_script script;
 int imageWidth;
 int imageHeight;
 
-const uchar4 *inPixels;
-uchar4 *outPixels;
+const int deltaX[] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int deltaY[] = {1, 1, 0, -1, -1, -1, 0, 1};
 
-const int matrixSize = 3;
-const float kernel[] = {
-		-1, -1, -1,
-		-1, 8, -1,
-		-1, -1, -1
-};
+void root(const uchar4 *v_in, uchar4 *v_out, const void *usrData, uint32_t x, uint32_t y) {
+	float4 output;
+	output.r = output.g = output.b = 0;
 
-void root(const int32_t *v_in, int32_t  *v_out, const void *usrData, uint32_t x, uint32_t y) {
-	for(int x = 0; x < imageWidth; x++){
-		float4 output;
-		output.r = output.g = output.b = 0;
-		int position[] = {-1, 0, 1};
-
-		for(int i = 0; i < matrixSize; ++i){
-		   for(int j = 0; j < matrixSize; ++j){
-				float4 neighbourPixel = rsUnpackColor8888(inPixels[x + *v_in + position[i] + position[j]]);
-				output.r += (neighbourPixel.r * kernel[i * matrixSize + j]);
-				output.g += (neighbourPixel.g * kernel[i * matrixSize + j]);
-				output.b += (neighbourPixel.b * kernel[i * matrixSize + j]);
-		   }
+	float weight = 8;
+	for (int i = 0; i < 8; ++i) {
+		if (x + deltaX[i] < 0 || x + deltaX[i] >= imageWidth ||
+			y + deltaY[i] < 0 || y + deltaY[i] >= imageHeight) {
+			weight--;
+			continue;
 		}
+        float4 neighbourPixel = rsUnpackColor8888(rsGetElementAt_uchar4(in, x + deltaX[i], y + deltaY[i]));
 
-		if (output.r > 1.0) output.r = 1.0f;
-		if (output.g > 1.0) output.g = 1.0f;
-		if (output.b > 1.0) output.b = 1.0f;
-
-		outPixels[x + *v_in] = rsPackColorTo8888(output.rgb);
+		output.r -= neighbourPixel.r;
+		output.g -= neighbourPixel.g;
+		output.b -= neighbourPixel.b;
 	}
+
+	float4 pixel = rsUnpackColor8888(*v_in);
+	output.r += weight * pixel.r;
+	output.g += weight * pixel.g;
+	output.b += weight * pixel.b;
+
+	output.r = output.g = output.b = (output.r + output.g + output.b) / 3.0;
+
+	*v_out = rsPackColorTo8888(output.rgb);
 }
 
 void filter() {
